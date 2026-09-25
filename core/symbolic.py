6617e61c4704a0765f72a0f0c2245b13c8ac1ed9
Anas Mefteh <anesmeftah03@gmail.com>
2026-09-25 18:00:01 +0100

    implement Symbolic Bound representation

--- core/symbolic.py
+++ core/symbolic.py
@@ -2,5 +2,24 @@ import numpy as np
 
 class SymbolicBound:
     """Represent affine bounds : A * x + b where x is the input"""
-    def __init__(self):
-        pass
+    def __init__(self , A , b):
+        self.A = A
+        self.b = b
+
+    @classmethod
+    def from_input(cls , input_dim):
+        """Create symbolic representation of the input"""
+        A = np.eye(input_dim)
+        b = np.zeros(input_dim)
+
+        return cls(A,b)
+
+
+    def __repr__(self):
+        return (
+            f"SymbolicBound(\n"
+            f"A = \n{self.A}\n"
+            f"b = \n{self.b}\n"
+        )
+
+    
---
+++ tests/test_sip.py
@@ -0,0 +1,6 @@
+from core.symbolic import SymbolicBound
+
+
+x = SymbolicBound.from_input(3)
+
+print(x)
